5d1e4be0230e0d1d80e6a3cc13100c1756e986fd
ivobenedito <ivobenedito@gmail.com>
2013-08-26 17:30:09 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Example of a Rails app with Mongoid and Backbone JS.
+Implements API/token authentication.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneAuth::Application.config.secret_token = '263ffc522431feae5c9f790f3699cec6f5c14566b136e5e6755a72d0a35ef66ec212cb71ffa1a334668f50294d771f18fac9bef6c484810e72817361cfa1fd93'
