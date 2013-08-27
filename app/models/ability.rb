b221c27b199069fafb5fb5b0b10e7411adc5d44c
ivobenedito <ivobenedito@gmail.com>
2013-08-27 17:24:14 +0100

    updated CanCan abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,7 +1,19 @@
 class Ability
+  include CanCan::Ability
+
   def initialize(user)
     user ||= User.new
 
     can :read, :all
+
+    if user.role? :admin
+      can :create, Article
+      can :update, Article do |article|
+        article && article.user == user
+      end
+      can :destroy, Article do |article|
+        article && article.user == user
+      end
+    end
   end
 end
