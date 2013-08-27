e75d2fd1eb5c6853ca1eb9bbb65f50fcb23004a3
ivobenedito <ivobenedito@gmail.com>
2013-08-27 01:38:53 +0100

    added API controllers

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,45 @@
+module Api
+  class SessionsController < Api::BaseController
+    before_filter :authenticate_user!, only: :destroy
+
+    def show
+      render json: { 
+        id: session[:session_id], 
+        user: current_user
+      }, status: :ok
+    end
+
+    def create
+      @user = User.find_for_database_authentication(email: params[:email])
+      return invalid_login_attempt unless @user.present?
+
+      if @user.valid_password?(params[:password])
+        @user.ensure_authentication_token!
+        sign_in(:user, @user)
+        render json: { 
+          success: true, 
+          id: session[:session_id],
+          user: @user
+        }, status: :created
+       return
+     end
+     invalid_login_attempt
+    end
+
+    def destroy
+      current_user.reset_authentication_token!
+      sign_out(current_user)
+      render json: { success: true }, status: :ok
+    end
+
+    protected
+
+    def invalid_login_attempt
+      warden.custom_failure!
+      render json: { 
+        success: false, 
+        message: "Error with your login or password" 
+      }, status: :unauthorized
+    end    
+  end
+end
