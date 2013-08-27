e7837fb29fb39b80a0a93dbb87b4bdced0fc8222
ivobenedito <ivobenedito@gmail.com>
2013-08-27 01:38:25 +0100

    added API & devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 BackboneAuth::Application.routes.draw do
   root to: 'welcome#index'
+  devise_for :users
+
+  namespace :api, defaults: { format: :json } do
+    resource :session, only: [:show, :create, :destroy]
+  end
 end
