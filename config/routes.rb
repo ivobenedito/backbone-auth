a01487b925d214c9bb8125559bae30f170f1c868
ivobenedito <ivobenedito@gmail.com>
2013-08-27 17:21:59 +0100

    added example protected page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 BackboneAuth::Application.routes.draw do
   root to: 'welcome#index'
+
   devise_for :users
+  get :protected, to: 'welcome#protected'
 
   namespace :api, defaults: { format: :json } do
     resource :session, only: [:show, :create, :destroy]
